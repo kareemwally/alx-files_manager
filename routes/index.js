b9e9552c7ed42616f60edaa8c343f1398fabd8ab
kareem <kareemwally@std.mans.edu.eg>
2024-12-11 19:36:09 +0200

    /

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = express.Router();
 
@@ -15,8 +15,5 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
