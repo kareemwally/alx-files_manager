4cea84591554a1756c13c42c2852486dd0cfa077
kareem <kareemwally@std.mans.edu.eg>
2024-12-11 20:32:56 +0200

    worker

--- worker.js
+++ worker.js
@@ -1,38 +1,38 @@
 import Bull from 'bull';
+import dbClient from './utils/db.js';
 import imageThumbnail from 'image-thumbnail';
 import fs from 'fs';
-import dbClient from './utils/db.js';
+import path from 'path';
 
 const fileQueue = new Bull('fileQueue');
 
-fileQueue.process(async (job) => {
+fileQueue.process(async (job, done) => {
   const { fileId, userId } = job.data;
-  
+
   if (!fileId) {
     throw new Error('Missing fileId');
   }
-  
+
   if (!userId) {
     throw new Error('Missing userId');
   }
 
   const filesCollection = dbClient.db.collection('files');
-  const file = await filesCollection.findOne({ 
-    _id: new dbClient.ObjectId(fileId), 
-    userId: new dbClient.ObjectId(userId) 
-  });
+  const file = await filesCollection.findOne({ _id: new dbClient.ObjectId(fileId), userId: new dbClient.ObjectId(userId) });
 
   if (!file) {
     throw new Error('File not found');
   }
 
   const sizes = [500, 250, 100];
-  
-  for (const width of sizes) {
-    const thumbnail = await imageThumbnail(file.localPath, { width });
-    const thumbnailPath = `${file.localPath}_${width}`;
-    await fs.promises.writeFile(thumbnailPath, thumbnail);
-  }
-});
+  const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
 
-export default fileQueue;
+  for (const size of sizes) {
+    const options = { width: size };
+    const thumbnail = await imageThumbnail(file.localPath, options);
+    const thumbnailPath = path.join(folderPath, `${path.basename(file.localPath)}_${size}`);
+    fs.writeFileSync(thumbnailPath, thumbnail);
+  }
+
+  done();
+});
